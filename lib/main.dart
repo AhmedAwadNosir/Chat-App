b96804e92020bddcef0b55b37ad681d9096ffefb
AhmedAwadNosir <ahmedalprens98@gmail.com>
2023-07-22 20:28:04 +0300

    configre firbase to app to use it services

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
-
 import 'package:chat_app/views/login_view.dart';
 import 'package:chat_app/views/register_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'constants.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const chatApp());
 }
 
